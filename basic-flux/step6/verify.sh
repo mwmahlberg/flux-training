bdff83972f635636787a12f1ab7f73a8dda806a7
Markus Mahlberg <markus.mahlberg@dacoso.com>
2024-07-05 15:22:35 +0200

    fix: copy and paste error

--- basic-flux/step6/verify.sh
+++ basic-flux/step6/verify.sh
@@ -4,8 +4,6 @@ set -e
 
 stat /root/mycluster/clusters/killercoda/podinfo/helm-repo.yaml
 
-git ls-files --other --exclude-standard --exit-code
-
 if [[ $(git ls-files --other --exclude-standard) ]]; then exit 1; fi
 
 kubectl get helmrepositories.source.toolkit.fluxcd.io podinfo
