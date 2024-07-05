79bcb6b31bf4f81fb0b0d1389be130357c0cbd27
Markus Mahlberg <markus.mahlberg@dacoso.com>
2024-07-05 16:05:16 +0200

    fix(flux-basic): typo in step7

--- basic-flux/step7/hint.md
+++ basic-flux/step7/hint.md
@@ -1,4 +1,4 @@
 In order to make sure that all replicas of the deployment are ready
 you need to check the deployment:
 
-`kubectl wait deployment/podinfo --for condition=Available=True --for jsonpath='{.status.availaeReplicas}'=2`{{copy}}
+`kubectl wait deployment/podinfo --for condition=Available=True --for jsonpath='{.status.availableReplicas}'=2`{{copy}}
--- basic-flux/step7/verify.sh
+++ basic-flux/step7/verify.sh
@@ -6,4 +6,4 @@ stat /root/mycluster/clusters/killercoda/podinfo/helm-release.yaml
 
 if [[ $(git ls-files --other --exclude-standard) ]]; then exit 1; fi
 
-kubectl wait deployment/podinfo --for condition=Available=True --for jsonpath='{.status.availaeReplicas}'=2
+kubectl wait deployment/podinfo --for condition=Available=True --for jsonpath='{.status.availableReplicas}'=2
