aa1805b3df6b7ceab054ddd24a04ee88e1a8d0cd
Markus Mahlberg <markus@mahlberg.io>
2024-08-01 15:52:19 +0200

    check

--- Go/workspaces/1_create_project/verify.sh
+++ Go/workspaces/1_create_project/verify.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
 set -e
+set -x
 
-go env > /root/goenv
 
-pushd /root/mycoolapp
 
-go run main.go &> filename
+stat ./go/pkg/mod/golang.org/x/example/hello@v0.0.0-20240716161537-39e772fc2670/hello.go || exit 1
+stat ./mycoolapp/main.go || exit 1
+stat ./mycoolapp/go.mod || exit 1
+stat ./mycoolapp/go.sum || exit 1
+
+pushd ./mycoolapp
+
+go run main.go || exit 1
 
 popd
